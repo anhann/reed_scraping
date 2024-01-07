361aa8dab9b2edb16c3b8f411edd6370ed164bd2
Anh Nguyen <74972572+anhann@users.noreply.github.com>
2024-01-07 13:06:40 +0000

    Update main.py

--- main.py
+++ main.py
@@ -283,7 +283,7 @@ class SkillRequired:
             skill_dict[i]+=1
 
 
-        red= ['experience', 'skill', 'knowledge', 'role', 'ability', 'excelent', 'each', 'job', 'understanding', 'application', 'all', 'use']
+        red= ['experience', 'skill', 'knowledge', 'role', 'ability', 'excelent', 'each', 'job', 'understanding', 'application', 'all', 'use', '']
         for word in red:
           if word in skill_dict.keys():
             del skill_dict[word]

c7c58dc4b23149781f4a2ea64742e572e9a9c503
Anh Nguyen <74972572+anhann@users.noreply.github.com>
2024-01-07 13:55:04 +0000

    Update main.py

--- main.py
+++ main.py
@@ -359,7 +359,7 @@ def main():
             job_api.plot_salary_ranges(data_df)
 
            
-        with st.expander("Compare Your Salary to Market"):
+        with st.container("Compare Your Salary to Market"):
             user_salary = st.number_input("Enter Your Salary", min_value=0)
 
             if st.button("Compare Salary"):
